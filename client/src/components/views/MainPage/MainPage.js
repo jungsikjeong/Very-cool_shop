93bf01b46d8a425d5b7672598b7744b6063b7063
jungsikjeong <wndtlr1024@gmail.com>
2021-05-21 17:53:23 +0900

    TEST

--- client/src/components/views/MainPage/MainPage.js
+++ client/src/components/views/MainPage/MainPage.js
@@ -1,22 +1,12 @@
 import React from 'react';
 import styled from 'styled-components';
 import SliderJS from './Sections/Slider/Slider';
-import bannerPc from '../../../assets/images/banner.png';
-import bannerM from '../../../assets/images/banner-m.png';
 
 import { useMediaQuery } from 'react-responsive';
 
 const Container = styled.div`
-  /* width: 100%; */
   color: black;
   overflow-x: hidden;
-`;
-
-const BannerWrap = styled.div`
-  img {
-    max-width: 100%;
-    width: 100%;
-  }
 `;
 
 const MainPage = () => {
@@ -26,14 +16,7 @@ const MainPage = () => {
 
   return (
     <Container>
-      <BannerWrap>
-        {isMobile ? (
-          <img src={bannerM} alt="배너 이미지" />
-        ) : (
-          <img src={bannerPc} alt="배너 이미지" />
-        )}
-      </BannerWrap>
-      {/* <SliderJS /> */}
+      <SliderJS />
     </Container>
   );
 };
